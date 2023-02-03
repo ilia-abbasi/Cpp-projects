4c780b2023437fef6cf02a3b9de9aa2e90540084
Ilia <81512402+ilia-abbasi@users.noreply.github.com>
2023-02-03 18:05:44 +0330

    Update vc encoder.cpp
    - Case sensitive encoding.
    - Intelligent file saving.

--- projects/Vigenere Cipher/Encode/vc encoder.cpp
+++ projects/Vigenere Cipher/Encode/vc encoder.cpp
@@ -5,12 +5,14 @@
 #include <conio.h>
 #include <string>
 #include <fstream>
+#include <sstream>
 using namespace std;
 
 char LOW[26] = { 'a' , 'b' , 'c' , 'd' , 'e', 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z'};
 char UPP[26] = { 'A' , 'B' , 'C' , 'D' , 'E', 'F' , 'G' , 'H' , 'I' , 'J' , 'K' , 'L' , 'M' , 'N' , 'O' , 'P' , 'Q' , 'R' , 'S' , 'T' , 'U' , 'V' , 'W' , 'X' , 'Y' , 'Z'};
 
 string encode(string text, string key);
+string save_file(string t);
 string upper(string text);
 int toint(char c);
 
@@ -45,27 +47,24 @@ string encode(string text, string key){
 		getline(file, k);
 	file.close();
 	
-	
-	t = upper(t);
 	k = upper(k);
-	
 	
 	for(register int i = 0, j = 0; i < t.size(); i++){
 		
-		if(t[i] == ' ')
-			continue;
-		
-		int x,y;
-		x = toint(t[i]);
+		int x,y,letter_case = 0;
+		x = (int)t[i];
 		y = toint(k[j]);
 		
-		if(x >= 1 and x <= 26 and y >= 1 and y <= 26){
+		if(x > 64 and x < 91)
+			letter_case = 90;
+		if(x > 96 and x < 123)
+			letter_case = 122;
+		if(!letter_case)
+			continue;
 		
-			x += y-1;
-			if(x > 26)
-				x -= 26;
-			x += 64;
-		}
+		x += y-1;
+		if(x > letter_case)
+			x -= 26;
 		
 		t[i] = (char)x;
 		
@@ -74,10 +73,7 @@ string encode(string text, string key){
 			j = 0;
 	}
 	
-	ofstream write;
-	write.open("result.txt");
-	write << t;
-	write.close();
+	save_file(t);
 	
 	return t;
 }
@@ -94,10 +90,44 @@ string upper(string text){
 	
 	string t = text;
 	
-	for(register int i = 0; i < t.size(); i++)
-		for(register int j = 0; j < 26; j++)
-			if(t[i] == LOW[j])
-				t[i] = UPP[j];
+	for(register int i = 0; i < t.size(); i++){
+		int x = (int)t[i];
+		if(x > 90)
+			x -= 32;
+		t[i] = (char)x;
+	}
 	
 	return t;
+}
+string save_file(string t){
+	
+	ofstream write;
+	ifstream read;
+	string name;
+	
+	int i = 0;
+	while(1){
+		
+		name = "result";
+		stringstream ss;
+		ss << i;
+		name += ss.str();
+		name += ".txt";
+		
+		i++;
+		read.open(name.c_str());
+		if(read){
+			
+			read.close();
+			continue;
+		}
+		read.close();
+		
+		write.open(name.c_str());
+		write << t;
+		write.close();
+		break;
+	}
+	
+	return name;
 }
