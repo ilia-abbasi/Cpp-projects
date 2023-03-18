a0e9c1349a0a395ead846ec214a8c853a07b4a91
Ilia <81512402+ilia-abbasi@users.noreply.github.com>
2023-03-18 11:38:32 +0330

    Update cc encoder.cpp
    - added the function raw_or_file() and used it in main().
    - added an infinite while loop for user input.
    - removed getch() and as a result <conio.h> library, as it is not used anymore.
    - made the function save_file() more general.
    - used stoi() function instead of sstream to prevent the devastating "continiuous file making" bug.
    - renamed the function encode() to a more specific name cc_encrypt().

--- projects/Caesar Cipher/Encode/cc encoder.cpp
+++ projects/Caesar Cipher/Encode/cc encoder.cpp
@@ -2,7 +2,6 @@
 // Programmer : Ilia Abbasi
 
 #include <iostream>
-#include <conio.h>
 #include <string>
 #include <fstream>
 using namespace std;
@@ -10,58 +9,60 @@ using namespace std;
 char LOW[26] = { 'a' , 'b' , 'c' , 'd' , 'e', 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z'};
 char UPP[26] = { 'A' , 'B' , 'C' , 'D' , 'E', 'F' , 'G' , 'H' , 'I' , 'J' , 'K' , 'L' , 'M' , 'N' , 'O' , 'P' , 'Q' , 'R' , 'S' , 'T' , 'U' , 'V' , 'W' , 'X' , 'Y' , 'Z'};
 
-string encode(string text, int key);
+string cc_encrypt(string text, int key);
+string raw_or_file(string s);
+string save_file(string t, string n);
 string upper(string text);
 
 int main(){
 	
-	string text;
+	string text, cipher, name, k;
 	int key;
 	
-	cout<<"Your text:\n\n";
-	getline(cin, text);
+	while(1){
+		cout<<"Your text:\n\n";
+		getline(cin, text);
+		cout<<"\nNumber of shifts:\n\n";
+		cin>> k;
+		cout<<"\nResult:\n\n";
+		
+		text = raw_or_file(text);
+		key = stoi(k);
+		cipher = cc_encrypt(text, key);
+		
+		cout << cipher << "\n\n";
+		cout << "Name of the file to be saved(! to cancel): ";
+		cin >> name;
+		cin.ignore();
+		system("cls");
+		if(name == "!")
+			continue;
+		
+		cout << "Saved as " << save_file(cipher, name) << "\n--------\n\n";
+	}
 	
-	cout<<"\nNumber of shifts:\n\n";
-	cin>> key;
-	
-	cout<<"\nResult:\n\n";
-	cout<< encode(text, key);
-	
-	getch();
+	return 0;
 }
 
-string encode(string text, int key){
+string cc_encrypt(string text, int key){
 	
-	string t = text;
+	text = upper(text);
 	
-	ifstream read;
-	read.open(text.c_str());
-	if(read)
-		getline(read,t);
-	read.close();
-	
-	t = upper(t);
-	
-	for(register int i = 0; i < t.size(); i++){
+	for(register int i = 0; i < text.size(); i++){
 		
-		if((int)t[i] < 65 or (int)t[i] > 90)
+		if((int)text[i] < 65 or (int)text[i] > 90)
 			continue;
 		
 		int temp;
-		temp = ((int)t[i]) - 65;
+		temp = ((int)text[i]) - 65;
 		temp += key;
 		temp = temp % 26;
 		temp += 65;
 		
-		t[i] = (char)temp;
+		text[i] = (char)temp;
 	}
 	
-	ofstream write;
-	write.open("result.txt");
-	write << t;
-	write.close();
-	
-	return t;
+	return text;
 }
 
 string upper(string text){
@@ -74,4 +75,52 @@ string upper(string text){
 				t[i] = UPP[j];
 	
 	return t;
+}
+
+string raw_or_file(string s){
+	
+	ifstream read;
+	read.open(s);
+	if(!read)
+		return s;
+	
+	string line;
+	s = "";
+	while(getline(read, line))
+		s = s + line + '\n';
+	s.pop_back();
+	
+	read.close();
+	return s;
+}
+
+string save_file(string t, string n){
+	
+	ofstream write;
+	ifstream read;
+	string name;
+	
+	int i = 0;
+	while(1){
+		
+		name = n;
+		if(i)
+			name += to_string(i);
+		name += ".txt";
+		
+		i++;
+		read.open(name.c_str());
+		if(read){
+			read.close();
+			continue;
+		}
+		read.close();
+		
+		write.open(name.c_str());
+		write << t;
+		write.close();
+		break;
+	}
+	
+	return name;
 }
